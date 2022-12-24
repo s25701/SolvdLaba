dd0fe798230e6ad0acbe14780091109b5e589f26
arzuk <93736885+s25701@users.noreply.github.com>
2022-12-24 17:06:49 +0100

    Delete src/main/java/SecondHw/ConcertHall/Mains directory

--- src/main/java/SecondHw/ConcertHall/Mains/TicketMain.java
+++
@@ -1,8 +0,0 @@
-//        while (ticket2.isStatus() == false) {
-//            try {
-//                System.out.println("\n"+ ticket2);
-//                break;
-//            } catch (Throwable e) {
-//                throw new TicketIsTakenException("The ticket is taken", e);
-//            }
-//        }
